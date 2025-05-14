4361489be660aabaf57d7c5e82762ba87d37a7d4
sam kabedi <samuelkabedi97@gmail.com>
2025-05-14 18:28:44 +0200

    first commit

---
+++ TABLES.sql/week6.sql
@@ -0,0 +1,13 @@
+SELECT e.firstName, e.lastName, e.email, o.officeCode
+FROM employees e
+INNER JOIN offices o ON e.officeCode = o.officeCode;
+
+SELECT p.productName, p.productVendor, p.productLine
+FROM products p
+LEFT JOIN productlines pl ON p.productLine = pl.productLine;
+
+SELECT o.orderDate, o.shippedDate, o.status, o.customerNumber
+FROM customers c
+RIGHT JOIN orders o ON c.customerNumber = o.customerNumber
+ORDER BY o.orderDate
+LIMIT 10;
--- answers.sql/ANSWER.sql
+++ answers.sql/ANSWER.sql
@@ -3,3 +3,4 @@ ALTER TABLE customers DROP INDEX IdxPhone;
 CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
 GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
 ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
+
